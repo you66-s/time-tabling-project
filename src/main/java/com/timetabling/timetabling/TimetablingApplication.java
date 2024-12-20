3f14307e72dc59b215fed4c313402f28a90724a4
Youssef achehboune <achehbouneyoussef2@gmail.com>
2024-12-20 22:10:00 +0100

    first commit

---
+++ src/main/java/com/timetabling/timetabling/TimetablingApplication.java
@@ -0,0 +1,13 @@
+package com.timetabling.timetabling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimetablingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimetablingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/timetabling/timetabling/TimetablingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.timetabling.timetabling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimetablingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
